073d4f68f18fa76772e20917d5d344112c126bb2
Kastro-59 <84104324+Kastro-59@users.noreply.github.com>
2023-06-15 23:01:41 +0200

    Add files via upload

---
+++ stream/index.php
@@ -0,0 +1,212 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Zeno-Nuevo</title>
+<script src="https://kastro-59.github.io/Poligonos/stream/js/jquery.min.js"></script>
+<script type="text/javascript" src="https://kastro-59.github.io/Poligonos/stream/js/lunaradio.min.js"></script>			
+</head>
+<body> 
+<style>
+ body { margin: 0; }
+ div {
+	position: fixed;
+	font-family: "Helvetica Neue","Helvetica","Arial",sans-serif;
+ }
+
+ .a {
+	left: 0;
+	top: 0;
+	right: 50%;
+	bottom: 50%;
+	width:420px; 
+	height:364px;
+	background-size: cover;
+ }
+
+  .disc{
+  width: 308px;            
+  height: 308px;          
+  background: url(https://i.imgur.com/FCJxb2D.png)center no-repeat;
+  background-size: cover;
+  position: absolute;
+  z-index: -10;
+  overflow: hidden;
+  left: 109px;
+  top: 12px;
+    -webkit-animation: spin 6s linear infinite;
+    -moz-animation: spin 6s linear infinite;
+   animation: spin 6s linear infinite;
+  z-index: -6;
+  
+   }
+   }
+
+    @-moz-keyframes spin {
+    100% {
+     -moz-transform: rotate(360deg);
+   }
+   }
+   @-webkit-keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+   }
+   @keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+   }
+
+   .data {
+     position: absolute;
+     font-family: 'Chela One', cursive;
+     font-size: 15px;
+     left: 120px;
+     top: 15px;
+     color: #6a0311;
+     width: 236px;
+     height: 19px;
+     z-index: 16;
+     border: 1px;
+  }
+   #Clock {
+     position: absolute;
+     top: 35px;
+     left: 200px;
+     width: 43px;
+     color: #6a0311;
+     font-family: 'Chela One', cursive;
+     font-size: 15px;
+  }
+
+</style>
+<center>
+ <style> 
+   <!-- 
+   body { background:url() repeat-x, url('') }*{ cursor:url(https://i.imgur.com/QpftHmt.png),auto} 
+   -->
+</style>
+</center> 
+<div align="center">
+ <div class="player" id="miplayer">
+ <div class="a" style="background-image:url(https://i.imgur.com/kLDcWI6.png)">
+
+ <div id="kastroplayer" style="width:420px; height:364px;background: transparent url() bottom / cover no-repeat;">loading...</div>	
+ 
+ <div class="disc">  
+</div>
+</head>
+<body>
+&nbsp;&nbsp; <span id="Clock">12:57:05</span>
+
+
+<script language="JavaScript">
+<!--
+var Elem = document.getElementById("Clock");
+function Horario(){
+var Hoje = new Date();
+var Horas = Hoje.getHours();
+if(Horas < 10){
+Horas = "0"+Horas;
+}
+var Minutos = Hoje.getMinutes();
+if(Minutos < 10){
+Minutos = "0"+Minutos;
+}
+var Segundos = Hoje.getSeconds();
+if(Segundos < 10){
+Segundos = "0"+Segundos;
+}
+Elem.innerHTML = Horas+":"+Minutos+":"+Segundos;
+}
+window.setInterval("Horario()",1000);
+//-->
+</script>
+<div id="data" class="data">
+<script language="javascript">
+
+var fecha=new Date();
+var diames=fecha.getDate();
+var diasemana=fecha.getDay();
+var mes=fecha.getMonth() +1 ;
+var ano=fecha.getFullYear();
+
+var textosemana = new Array (7);
+textosemana[0]="Dom";
+textosemana[1]="Lun";
+textosemana[2]="Mar";
+textosemana[3]="Mie";
+textosemana[4]="Jue";
+textosemana[5]="Vie";
+textosemana[6]="Sab";
+
+var textomes = new Array (12);
+textomes[1]="Ene";
+textomes[2]="Feb";
+textomes[3]="Mar";
+textomes[4]="Abr";
+textomes[5]="May";
+textomes[6]="Jun";
+textomes[7]="Jul";
+textomes[8]="Ago";
+textomes[9]="Sep";
+textomes[10]="Oct";
+textomes[11]="Nov";
+textomes[12]="Dic";
+
+document.write("" + textosemana[diasemana] + " " + diames + " de " + textomes[mes] + " de " + ano + "<br>");
+
+</script>
+
+
+<script>
+jQuery(document).ready(function($) {
+	$("#kastroplayer").lunaradio({
+		token: "ZFNWW2FiXlNrV2QgXltgXQ==",
+  userinterface: "big",
+  backgroundcolor: "transparent",
+  fontcolor: "#6a0311",
+  hightlightcolor: "#fa225b",
+  fontname: "Saira Condensed",
+  googlefont: "Saira+Condensed:wght@100",
+  fontratio: "0.5",
+  radioname: "DINARAMA-RADIO",
+  scroll: "false",
+  coverimage: "https://i.imgur.com/TSc5ws2.gif",
+  usevisualizer: "fake",
+  coverstyle: "animated",
+  visualizertype: "4",
+  multicolorvisualizer: "true",
+    color1: "#f6f6f6",
+    color2: "#323edd",
+    color3: "#fc5404",
+    color4: "#fa225b",
+		visualizeropacity: "0.4",
+		itunestoken: "1000lIPN",
+		metadatatechnic: "stream-icy-meta",
+		ownmetadataurl: "",
+		streamurl: "https://stream.zeno.fm/emertvc73mruv",
+		streamtype: "radiojar",
+		icecastmountpoint: "",
+		radionomyid: "",
+		radionomyapikey: "",
+		radiojarid: "",
+		radiocoid: "",
+		shoutcastpath: "",
+		shoutcastid: "",
+		streamsuffix: "",
+		metadatainterval: "10000",
+		volume: "50",
+		debug: "false",
+		autoplay: "",
+		displayliveicon: "true",
+		displayvisualizericon: "true",
+	usestreamcorsproxy: "true", 
+	corsproxy: "",
+	});
+});
+</script>
+</body>		
+</html>
