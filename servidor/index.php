ac723cb8f710071b0c0c24ddcb4e0225bb1f4706
Kastro-59 <84104324+Kastro-59@users.noreply.github.com>
2023-06-23 00:25:34 +0200

    Add files via upload

--- servidor/index.php
+++ servidor/index.php
@@ -3,9 +3,10 @@
 <head>
 	<meta name="color-scheme" content="light dark">
 </head>
-<body>	
-<iframe src="https://zenoplay.zenomedia.com/api/zenofm/nowplaying/emertvc73mruv?_=stream-icy-meta.php?" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
-
+<body>
+<div>	
+<iframe src="https://zenoplay.zenomedia.com/api/zenofm/nowplaying/emertvc73mruv?_=" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+</div>
 
 </body>
 </html>
