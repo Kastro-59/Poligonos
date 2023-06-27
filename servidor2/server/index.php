a50e48f00512a66f5151057fcbf882730e234923
Kastro-59 <84104324+Kastro-59@users.noreply.github.com>
2023-06-27 17:48:14 +0200

    Update index.php

--- servidor2/server/index.php
+++ servidor2/server/index.php
@@ -9,7 +9,5 @@
 <div>
 	<pre style="word-wrap: break-word; white-space: pre-wrap;">{"duration":0,"thumb":"","artist":"","buy_urls":"","album":null,"show":{},"guid":null,"sku":null,"title":"","info_urls":""}</pre>
 </div>
-
-<div style="text-align:right;position:fixed;z-index:9999999;bottom:0;width:auto;right:1%;cursor:pointer;line-height:0;display:block!important"></div>
 </body>
 </html>
