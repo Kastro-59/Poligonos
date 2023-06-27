db9d2d62808d29ec3c315814b68231862d7589d1
Kastro-59 <84104324+Kastro-59@users.noreply.github.com>
2023-06-27 17:01:55 +0200

    Add files via upload

---
+++ servidor2/server/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="color-scheme" content="light dark">
+</head>
+<body>
+	
+<iframe src="https://zenoplay.zenomedia.com/api/zenofm/nowplaying/emertvc73mruv" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+<div>
+	<pre style="word-wrap: break-word; white-space: pre-wrap;">{"duration":0,"thumb":"","artist":"","buy_urls":"","album":null,"show":{},"guid":null,"sku":null,"title":"","info_urls":""}</pre>
+</div>
+
+<div style="text-align:right;position:fixed;z-index:9999999;bottom:0;width:auto;right:1%;cursor:pointer;line-height:0;display:block!important"></div>
+</body>
+</html>
