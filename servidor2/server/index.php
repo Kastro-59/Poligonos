4d800bd0ba187d7bc24ffd55ac5f9b21b731c746
Kastro-59 <84104324+Kastro-59@users.noreply.github.com>
2023-06-27 17:51:04 +0200

    Update index.php

--- servidor2/server/index.php
+++ servidor2/server/index.php
@@ -6,8 +6,6 @@
 <body>
 	
 <iframe src="https://zenoplay.zenomedia.com/api/zenofm/nowplaying/emertvc73mruv" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
-<div>
-	<pre style="word-wrap: break-word; white-space: pre-wrap;">{"duration":0,"thumb":"","artist":"","buy_urls":"","album":null,"show":{},"guid":null,"sku":null,"title":"","info_urls":""}</pre>
-</div>
+
 </body>
 </html>
